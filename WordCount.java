f25db851e5286ad372964df4f07f7b4b178d8487
supernovaeee <azzasekarnovaa@gmail.com>
2023-04-29 21:28:35 +0800

    Fix errors with solution2.java

--- WordCount.java
+++ WordCount.java
@@ -11,14 +11,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class solution2 {
+public class WordCount {
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "total max min");
-    job.setJarByClass(solution2.class);
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
-    //job.setCombinerClass(IntSumReducer.class);
+    job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
@@ -31,47 +31,31 @@ public class solution2 {
        extends Mapper<Object, Text, Text, IntWritable>{
 
     private final static IntWritable one = new IntWritable(1);
-    private static IntWritable valueInt = new IntWritable();
-    private Text cityText = new Text();
-    private Text stateText = new Text();
+    private Text word = new Text();
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      String[] fields = value.toString().split(",");
-      cityText.set(fields[1].trim());
-      stateText.set(fields[0].trim());
-      int count = Integer.parseInt(fields[2].trim());
-      valueInt.set(count);      
-
-      context.write(stateText, valueInt);
-     
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
     }
   }
 
   public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,Text> {
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
     private IntWritable result = new IntWritable();
 
     public void reduce(Text key, Iterable<IntWritable> values,
                        Context context
                        ) throws IOException, InterruptedException {
-
-        int total = 0;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (IntWritable val : values) {
-            int count = val.get();
-            total += count;
-            if (count > max){
-                max = count;
-            }
-            if(count < min){
-                min = count;
-            }
-        }
-        String result = String.format("%d\t%d\t%d", total, max, min);
-
-      context.write(key, new Text(result));
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
     }
   }
 }
